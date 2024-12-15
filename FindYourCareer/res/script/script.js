cdf70fca5cd6742b2a0a426d646d83ca2a7533eb
vignesh-web-developer <vigneshvicke21@gmail.com>
2024-12-15 22:50:00 +0530

    Restore local changes after OS change

--- FindYourCareer/res/script/script.js
+++ FindYourCareer/res/script/script.js
@@ -1,15 +1,59 @@
-document.querySelectorAll(".courseBtn").forEach((button) => {
-  const courseList = button.nextElementSibling;
-  button.addEventListener("click", function (event) {
-    event.stopPropagation();
-    courseList.classList.toggle("show");
-    button.classList.toggle("active");
+// course and contact bg change effect
+const dropdownList = document.querySelectorAll(".dropdown");
+dropdownList.forEach((item) => {
+  let course = item.previousElementSibling;
+
+  item.addEventListener("mouseenter", () => {
+    course.classList.add("active");
   });
 
-  document.addEventListener("click", function (event) {
-    if (!button.contains(event.target)) {
-      button.classList.remove("active");
-      courseList.classList.remove("show");
+  item.addEventListener("mouseleave", () => {
+    course.classList.remove("active");
+  });
+});
+
+//slides flip
+document.addEventListener("DOMContentLoaded", () => {
+  let newUser = document.querySelector(".new-user");
+
+  let loginSlide = document.querySelector(".login_slide");
+  let signupSlide = document.querySelector(".signup_slide");
+  newUser.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (
+      loginSlide.classList.contains("visible") &&
+      loginSlide.classList.contains("visibleMove")
+    ) {
+      loginSlide.classList.remove("visible", "visibleMove");
+      signupSlide.classList.remove("vanish", "vanishMove");
     }
+
+    signupSlide.classList.add("visible");
+    loginSlide.classList.add("vanish");
+    setTimeout(() => {
+      signupSlide.classList.add("visibleMove");
+      loginSlide.classList.add("vanishMove");
+    }, 900);
+  });
+
+  let loginSlideLink = document.getElementById("login_slide_link");
+  loginSlideLink.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (
+      signupSlide.classList.contains("visible") &&
+      signupSlide.classList.contains("visibleMove")
+    ) {
+      signupSlide.classList.remove("visible", "visibleMove");
+      loginSlide.classList.remove("vanish", "vanishMove");
+    }
+    signupSlide.classList.add("vanish");
+    loginSlide.classList.add("visible");
+
+    setTimeout(() => {
+      loginSlide.classList.add("visibleMove");
+      signupSlide.classList.add("vanishMove");
+    }, 900);
   });
 });
